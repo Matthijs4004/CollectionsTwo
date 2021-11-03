7370feb0383c29268b124b6f431e0ddb8552c00a
Matthijs4004 <matthijs4004@gmail.com>
2021-11-03 10:27:40 +0100

    add finished boodschappenlijst.py

---
+++ boodschappenlijst.py
@@ -0,0 +1,28 @@
+
+
+boodschap = 0
+
+itemList = []
+itemDict = {}
+while boodschap == 0:
+    item = input("Welk product heeft u nodig? ")
+    qty = int(input("Hoeveel "+ item +" heeft u nodig? "))
+    for i in range(qty):
+        if not item in itemDict:
+            itemDict[item] = 1
+        else:
+            itemDict[item] += 1
+    print(itemDict)
+    
+    question = input("Wil je nog iets op het boodschappenlijstje zetten? ")
+    if question == "nee":
+        print("\nDit is uw boodschappenlijstje:\n" + str(itemDict))
+        break
+
+
+
+
+        
+
+
+
